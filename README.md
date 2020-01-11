0ca1b4ff03d39c4b56fc9acd02bef573b526dfb8
ThisIsNsh <hadanis.singh@gmail.com>
2020-01-11 19:14:49 +0530

    Files Added

---
+++ README.md
@@ -0,0 +1,26 @@
+# MATLAB-Image-Blur
+
+### Image Processing
+
+Blur a colour image with different intensities.
+--------
+* I have taken average of `x` surrounding pixel.
+* The function takes 2 parameters `arr` & `times`. `arr` is the image array & `times` is the intensity of blur. 
+* I have set that average back to `x` surronding pixels.
+* I type casted `uint8` pixel intensities to `double` to allow values greater that `255` while adding intensities to calculate average.
+* `x` is a variable named `howmuch` in code. We can modify it according to our need. 
+
+### Original Image 
+<img src="pic.png" width="25%">
+
+### Blur Image with Intensity 1
+<img src="blur1.PNG">
+
+### Blur Image with Intensity 2
+<img src="blur2.PNG">
+
+### Blur Image with Intensity 3
+<img src="blur3.PNG">
+
+### Blur Image with Intensity 4
+<img src="blur4.PNG">
---
+++ blur.m
@@ -0,0 +1,42 @@
+﻿function im = blur(arr,times)
+    for a = 1:times
+        for row = 3:2:size(arr,1)-3
+            for col = 3:2:size(arr,2)-3 
+                arr = intensity(row,col,arr);
+            end
+        end
+    end
+    im = arr;
+end
+
+function x = intensity(i,j,arr)
+    
+    sum1=double(0);
+    sum2=double(0);
+    sum3=double(0);
+    
+    howmuch = 1;
+    factor = (howmuch+howmuch+1)*(howmuch+howmuch+1);
+    
+    for a = i-howmuch:i+howmuch
+        for b = j-howmuch:j+howmuch
+            sum1=sum1+double(arr(a,b,1));
+            sum2=sum2+double(arr(a,b,2));
+            sum3=sum3+double(arr(a,b,3));
+        end
+    end
+    
+    sum1=fix(sum1/factor);
+    sum2=fix(sum2/factor);
+    sum3=fix(sum3/factor);
+    
+    for a = i-howmuch:i+howmuch
+        for b = j-howmuch:j+howmuch
+            arr(a,b,1) = sum1;
+            arr(a,b,2) = sum2;
+            arr(a,b,3) = sum3;    
+        end
+    end
+    
+    x = arr;
+end
